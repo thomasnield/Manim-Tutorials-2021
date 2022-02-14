07853a28f49c27d912b6ca8c16b44353830353da
Thomas Nield <thomasnield@live.com>
2022-02-13 19:34:01 -0600

    Add files via upload

---
+++ Tute4Code1.py
@@ -0,0 +1,15 @@
+from sympy import *
+
+# 4x + 2y + 4z = 44
+# 5x + 3y + 7z = 56
+# 9x + 3y + 6z = 72
+
+A = Matrix([
+    [4, 2, 4],
+    [5, 3, 7],
+    [9, 3, 6]
+])
+
+identity =  A.inv() * A
+
+print(identity)
